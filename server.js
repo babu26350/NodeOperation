56b731878b9a4893647809a7130daf835f295f25
babu26350 <bj167745@gmail.com>
2025-06-04 11:11:35 +0530

    set

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.use((req, res) => {
   res.status(404).json({ error: 'Route not found11' });
 });
 // Start server
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
